cec3bb744e6c2826f4b0abcad3a38392c2e8f8e3
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2015-05-12 09:14:30 +0700

    Initial Commit

---
+++ KataTennis/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KataTennis
+//
+//  Created by Chanintorn Asavavichairoj on 5/12/2558 BE.
+//  Copyright (c) 2558 JO. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
